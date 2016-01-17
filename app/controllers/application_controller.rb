b722bcde2e01ff3d3c811bdcf409f52d7d1f79b2
Greg Donald <gdonald@gmail.com>
2016-01-16 20:28:26 -0600

    add current_user checking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def require_login
+    if session[:user_id].nil?
+      flash[:error] = 'Login required'
+      redirect_to login_path
+      return false
+    end
+    @current_user = User.where(:id => session[:user_id]).first
+  end
+
 end
